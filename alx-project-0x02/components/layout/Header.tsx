5e4cee4a1bccf2d7d84dcc5bfdc25995c663fa48
Ndata <wisdomndata@gmail.com>
2025-06-07 16:35:11 +0100

    Project Setup

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+// components/layout/Header.tsx
+
+const Header = () => {
+  return (
+    <header className="bg-blue-600 text-white p-4 text-center">
+      <h1 className="text-2xl font-bold">Welcome to ALX Project 2</h1>
+    </header>
+  );
+};
+
+export default Header;
