9120dc3cddd66bc98e51e1571598fb80a556856b
Ndata <wisdomndata@gmail.com>
2025-06-07 16:52:40 +0100

    Project

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,10 +1,13 @@
-// components/layout/Header.tsx
-
+import Link from "next/link";
 const Header = () => {
   return (
+    <div className="flex flex-col items-center justify-center">
     <header className="bg-blue-600 text-white p-4 text-center">
       <h1 className="text-2xl font-bold">Welcome to ALX Project 2</h1>
     </header>
+     <Link href="/home" className="hover:underline">Home</Link>
+     <Link href="/about" className="hover:underline">About</Link>
+     </div>
   );
 };
 
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
     >
-      <h1>Welcome</h1>
+      <h1>Wisdom</h1>
     </div>
   );
 }
